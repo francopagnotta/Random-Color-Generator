a6ef0dae35643a01166a480482c298984940e7b4
FrancoPagnotta <francoapagnotta@gmail.com>
2021-07-20 00:12:48 -0300

    a transition was added to colorValue element in the js file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const body = document.body;
-const colorValue = document.querySelector('.color-value')
+const colorValue = document.querySelector('.color-value');
 const button = document.querySelector('.btn');
 
 // RANDOM COLOR
@@ -22,20 +22,21 @@ button.addEventListener('click',()=>{
 	const newColor = generateRandomColor();
 
 	body.style.background = newColor;
-	body.style.transition = `.5s ease-in-out`
-
+	body.style.transition = `.5s ease-in-out`;
+	
 	colorValue.textContent = newColor;
 	colorValue.style.color = newColor;
+	colorValue.style.transition = `.5s ease-in-out`;
 });
 
 button.addEventListener('mousedown',()=> {
 	button.style.transform = 'scale(.99)';
-	button.style.boxShadow = `inset 6px 6px 10px #292929, inset -6px -6px 10px #373737`
+	button.style.boxShadow = `inset 6px 6px 10px #292929, inset -6px -6px 10px #373737`;
 	button.style.color = `#c8c8c8`
 })
 
 button.addEventListener('mouseup',()=> {
 	button.style.transform = 'scale(1)';
-	button.style.boxShadow = null
-	button.style.color = `var(--color-light)`
+	button.style.boxShadow = null;
+	button.style.color = `var(--color-light)`;
 })
